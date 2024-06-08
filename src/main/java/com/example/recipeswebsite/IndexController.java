cc146c98e3b56e776ba92a19b16ae34d932b894f
AtomicAntt <anthonychi36@gmail.com>
2024-06-07 23:45:12 -0700

    Initialize control panel for admin use

--- src/main/java/com/example/recipeswebsite/IndexController.java
+++ src/main/java/com/example/recipeswebsite/IndexController.java
@@ -4,7 +4,6 @@ import com.example.recipeswebsite.Model.User;
 import com.example.recipeswebsite.Repositories.UserRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,25 +22,41 @@ public class IndexController {
     @RequestMapping("/")
     public String index(Model model, @CookieValue(value="username", defaultValue="n/a") String username, @CookieValue(value="password", defaultValue="n/a") String password){
         User user = userService.authenticateUser(username, password);
-
-        boolean isAdmin = false;
-
         if (user != null){
-            isAdmin = user.isAdmin();
+            model.addAttribute("isAdmin", user.isAdmin());
         }
 
-        model.addAttribute("isAdmin", isAdmin);
         return "main";
     }
 
     @GetMapping("/about")
-    public String about(Model model){
+    public String about(Model model, @CookieValue(value="username", defaultValue="n/a") String username, @CookieValue(value="password", defaultValue="n/a") String password){
+        User user = userService.authenticateUser(username, password);
+        if (user != null){
+            model.addAttribute("isAdmin", user.isAdmin());
+        }
         return "about";
     }
 
     @GetMapping("/login")
-    public String login(Model model){
+    public String login(Model model, @CookieValue(value="username", defaultValue="n/a") String username, @CookieValue(value="password", defaultValue="n/a") String password){
+        User user = userService.authenticateUser(username, password);
+        if (user != null){
+            model.addAttribute("isAdmin", user.isAdmin());
+        }
         return "login";
+    }
+
+    @GetMapping("/controlpanel")
+    public String controlpanel(Model model, @CookieValue(value="username", defaultValue="n/a") String username, @CookieValue(value="password", defaultValue="n/a") String password){
+        User user = userService.authenticateUser(username, password);
+        if (user != null){
+            model.addAttribute("isAdmin", user.isAdmin());
+            if (user.isAdmin()){
+                return "controlpanel";
+            }
+        }
+        return "main"; // replace with error page later
     }
 
     @PostMapping("/login")
